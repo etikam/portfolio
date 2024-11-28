15f2067fd6d466869b410123ccb45dda6973e7d0
= <=>
2024-11-28 15:24:15 +0000

    adding things in .env.exempl

--- config/settings_prod.py
+++ config/settings_prod.py
@@ -1,8 +1,25 @@
 from __future__ import annotations
 
+import os
+
+import dj_database_url
+import environ
+
 from .settings import *
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
-SECRET_KEY = "6B59CB94F8B4F74BC1868B61AF8F4"
+# Set the project base directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+SECRET_KEY = env("SECRET_KEY")
+
 DEBUG = False
-ALLOWED_HOSTS = ["127.0.0.1", "192.168.1.198",'localhost']
+
+ALLOWED_HOSTS = env("ALLOWED_HOSTS")
+
+DATABASES = {'default': dj_database_url.config(conn_health_checks=True)}
