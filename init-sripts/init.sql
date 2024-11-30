367f7fe8522430bfb668be9224ca5b381673d6c2
= <=>
2024-11-30 21:11:48 +0000

    setting up traefic configuration in compose.yml

---
+++ init-sripts/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE portfolio_dev;
+CREATE DATABASE portfolio_test;
+GRANT ALL PRIVILEGES ON DATABASE portfolio_dev TO portfolio;
+GRANT ALL PRIVILEGES ON DATABASE portfolio_test TO portfolio;
